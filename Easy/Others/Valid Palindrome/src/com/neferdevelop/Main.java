5002752d4175288fa1b7ca91a22e4937ed7c802c
NeferDevelop <sergei.klimov814@gmail.com>
2023-10-22 15:05:51 +0300

    Initial Commit

---
+++ Easy/Others/Valid Palindrome/src/com/neferdevelop/Main.java
@@ -0,0 +1,47 @@
+package com.neferdevelop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string to check if it's a palindrome:");
+        String input = scanner.nextLine();
+
+        boolean result = isPalindrome(input);
+
+        if (result) {
+            System.out.println("The input is a palindrome.");
+        } else {
+            System.out.println("The input is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
+
